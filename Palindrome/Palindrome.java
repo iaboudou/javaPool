66f3ff12bd2cb93fb6a3c24933c760b4fdaa7c76
iaboudou <ilyassaboudou07@gmail.com>
2026-08-23 16:10:41 +0100

    w1 q1 Palindrom'

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,8 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
         StringBuffer str = new StringBuffer(s);
         str = str.reverse();
         return (str.toString().toLowerCase()).equals(s.toLowerCase());
