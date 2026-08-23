3f34b73052e4fae536d3f64ba269b99cc6f2a4d4
iaboudou <ilyassaboudou07@gmail.com>
2026-08-23 13:20:58 +0100

    w1 q1 StringConcat'

--- StringConcat/ExerciseRunner.java
+++ StringConcat/ExerciseRunner.java
@@ -1,6 +1,6 @@
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(StringConcat.concat("Hello ", "étudiant !"));
+        System.out.println(StringConcat.concat(null, "étudiant !"));
         System.out.println(StringConcat.concat("", "Hello World !"));
     }
 }
