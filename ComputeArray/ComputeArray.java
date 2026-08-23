aea9fa04e69e8c68b81f6e366d31126ecdd00f68
iaboudou <ilyassaboudou07@gmail.com>
2026-08-23 16:28:39 +0100

    w1 q1 ComputeArray'

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,5 +1,10 @@
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
+
+        if (array == null) {
+            return null;
+        }
+
         for (int i=0; i < array.length; i++) {
             if (array[i] % 3 == 0) {
                 array[i] *= 5;
