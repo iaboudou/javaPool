1436b29ff4a21bc8354e7592fa4b001ef919f949
iaboudou <ilyassaboudou07@gmail.com>
2026-08-23 13:39:29 +0100

    w1 q1 StringContains'

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -1,5 +1,9 @@
 public class StringContains {
     public static boolean isStringContainedIn(String subString, String s) {
-        return s.contains(subString);
+        try {
+            return s.contains(subString);
+        }catch(NullPointerException e) {
+            return false;
+        }
     }
 }
