5ea2ec5ca5e34f51980b1a8f2ef5838037a5a8f7
iaboudou <ilyassaboudou07@gmail.com>
2026-08-24 15:04:52 +0100

    w1 q2 'Capitalize'

---
+++ Capitalize/Capitalize.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Capitalize {
+    public static void capitalize(String[] args) throws IOException {
+        if (args.length != 2) {
+            return;
+        }
+        java.io.FileInputStream in = new java.io.FileInputStream(args[0]);
+        java.io.FileOutputStream out = new java.io.FileOutputStream(args[1]);
+        int r = 0;
+        byte[] d = new byte[1024];
+
+        while( (r = in.read(d) ) != -1) {
+            String cap = capitalizeString(new String(d));
+            out.write(cap.getBytes(), 0, r);
+        }
+    }
+
+    static String capitalizeString(String input) {
+
+        String res = "";
+        for (int i=0; i<input.length(); i++) {
+            if (
+                i == 0 
+                || ( i > 0 && String.format("%s", input.charAt(i)).matches("[a-z]") && Character.isWhitespace(input.charAt(i-1))) 
+                ) {
+                res += (char)(input.charAt(i) + 'A' - 'a');
+            }else {
+                res += input.charAt(i);
+            }
+        }
+
+        return res;
+    }
+}
