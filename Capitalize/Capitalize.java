016f63faa899f406261439b8877a5dd89704780e
iaboudou <ilyassaboudou07@gmail.com>
2026-08-24 15:44:54 +0100

    w1 q2 'Capitalize'

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -8,7 +8,7 @@ public class Capitalize {
         java.io.FileInputStream in = new java.io.FileInputStream(args[0]);
         java.io.FileOutputStream out = new java.io.FileOutputStream(args[1]);
         int r = 0;
-        byte[] d = new byte[1024];
+        byte[] d = new byte[10000];
 
         while( (r = in.read(d) ) != -1) {
             String cap = capitalizeString(new String(d));
@@ -20,24 +20,21 @@ public class Capitalize {
     }
 
     static String capitalizeString(String input) {
-
         String res = "";
         for (int i=0; i<input.length(); i++) {
-            if (
-                (i == 0 && String.format("%s", input.charAt(i)).matches("[a-z]")) 
-                || ( i > 0 && String.format("%s", input.charAt(i)).matches("[a-z]") && Character.isWhitespace(input.charAt(i-1))) 
-                ) {
-                res += (char)(input.charAt(i) + 'A' - 'a');
-            }else {
 
-                if (String.format("%s", input.charAt(i)).matches("[A-Z]")) {
+            boolean isUpperCase = String.format("%s", input.charAt(i)).matches("[A-Z]");
+            boolean isLowerCase = String.format("%s", input.charAt(i)).matches("[a-z]");
+            boolean previousCharisWhiteSpace = i == 0 ? true : Character.isWhitespace(input.charAt(i-1));
+
+            if ( (i == 0 && isLowerCase) || (i > 0 &&  previousCharisWhiteSpace && isLowerCase)  ) {
+                res += (char)(input.charAt(i) + 'A' - 'a');
+            }else if (i > 0 && !previousCharisWhiteSpace && isUpperCase) {
                     res += (char)(input.charAt(i) + 'a' - 'A' );
-                }else {
-                    res += input.charAt(i);
-                }
+            }else {
+                res += input.charAt(i);
             }
         }
-
         return res;
     }
 }
