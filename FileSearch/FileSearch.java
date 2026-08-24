da350e87db60f742a8dbcf8d57de399c26da60f7
iaboudou <ilyassaboudou07@gmail.com>
2026-08-24 17:20:10 +0100

    w1 q2 'SearchFile'

---
+++ FileSearch/FileSearch.java
@@ -0,0 +1,26 @@
+public class FileSearch {
+    public static String searchFile(String fileName) {
+        java.io.File obj = new java.io.File("FileSearch/documents");
+        obj = findFile(obj, fileName);
+        return obj == null ? null : obj.getPath();
+    }
+
+    static java.io.File findFile(java.io.File currentEntry, String searchedFile) {
+
+        if (currentEntry.isDirectory()) {
+            for (java.io.File e : currentEntry.listFiles()) {
+                String name = e == null ? null : e.getName();
+                if (name.equals(searchedFile)) {
+                    return e;
+                }else {
+                    java.io.File f = findFile(e, searchedFile);
+                    if (f != null && f.getName().equals(searchedFile)) {
+                        return f;
+                    }
+                }
+                System.out.println(name);
+            }
+        }
+        return currentEntry ;
+    }
+}
