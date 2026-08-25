169b23801524227af5a57dce9dfc0deba8783933
iaboudou <ilyassaboudou07@gmail.com>
2026-08-25 11:43:47 +0100

    w1 q3 'StarStatic'

--- StarStatic/CelestialObject.java
+++ StarStatic/CelestialObject.java
@@ -22,4 +22,35 @@ public class CelestialObject {
     public static double getDistanceBetweenInKm(CelestialObject a, CelestialObject b) {
         return getDistanceBetween(a, b) * KM_IN_ONE_AU;
     }
+
+        public double getX() {
+        return this.x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+    
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    
+    public double getZ() {
+        return this.z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
