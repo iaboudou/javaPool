12d24c8798026d463c9324fd1784b4357093d812
iaboudou <ilyassaboudou07@gmail.com>
2026-08-25 13:18:09 +0100

    w1 q3 'StarInheritance'

---
+++ StarInheritance/Star.java
@@ -0,0 +1,11 @@
+public class Star extends StarUtils.CelestialObject {
+    private double magnitude;
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+    public void getMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+}
--- StarUtils/CelestialObject.java
+++ StarUtils/CelestialObject.java
@@ -1,3 +1,5 @@
+package StarUtils;
+
 public class CelestialObject {
     public double x, y, z;
     public String name;
