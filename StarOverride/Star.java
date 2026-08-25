fc59320b9c6e5ea43fff30973a7aa0b1daa3a102
iaboudou <ilyassaboudou07@gmail.com>
2026-08-25 13:49:59 +0100

    w1 q3 'StarOverride'

---
+++ StarOverride/Star.java
@@ -0,0 +1,31 @@
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public Star() {
+        super();
+    }
+    public Star(String name, double x, double y, double z, double magnitude) {
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public String toString() {
+        return String.format("%s shines at the %.3f magnitude", name, this.magnitude);
+    }
+
+    public boolean equals(CelestialObject c) {
+        return c == null ? false : this.x == c.x && this.y == c.y && this.z == c.z && this.name.equals(c.name);
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(name, x, y, z);
+    }
+
+}
