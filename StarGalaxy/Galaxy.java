6be2c50aebe18cce03c7c4f0fdd7d199520716ab
iaboudou <ilyassaboudou07@gmail.com>
2026-08-25 17:38:56 +0100

    w1 q3 'StarGalaxy'

---
+++ StarGalaxy/Galaxy.java
@@ -0,0 +1,15 @@
+public class Galaxy {
+    java.util.List<CelestialObject> celestialObjects;
+
+    public Galaxy(){
+        celestialObjects = new java.util.ArrayList<CelestialObject>();
+    }
+
+    public java.util.List<CelestialObject> getCelestialObjects() {
+        return celestialObjects;
+    }
+
+    public void addCelestialObject(CelestialObject arg) {
+        celestialObjects.add(arg);
+    }
+}
