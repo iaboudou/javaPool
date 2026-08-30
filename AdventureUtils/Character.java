2e3a4bc436e97073bdc408cace532c2d3bd57c5a
iaboudou <ilyassaboudou07@gmail.com>
2026-08-30 12:04:55 +0100

    w2 q1 AdventureUtils

---
+++ AdventureUtils/Character.java
@@ -0,0 +1,83 @@
+public class Character {
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+    private static java.util.List<Character> allCharacters = new java.util.ArrayList<>();
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.maxHealth = this.currentHealth = maxHealth;
+        allCharacters.add(this);
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return currentHealth == 0 
+            ? String.format("%s : KO", name) 
+            : String.format("%s : %d/%d", name, currentHealth, maxHealth );
+    }
+
+    public void takeDamage(int arg) {
+        currentHealth = currentHealth - arg;
+        if (currentHealth  < 0) {
+            currentHealth = 0;
+        }
+    }
+
+    public void attack(Character name) {
+        name.takeDamage(9);
+    }
+
+    public static String printStatus() {
+        String res = "------------------------------------------\n";
+        if (allCharacters.size() == 0) {
+            res += "Nobody's fighting right now !\n";
+        }else {
+            res += "Characters currently fighting :\n";
+        }
+        for (Character c : allCharacters) {
+            res += " - " + c.toString() + "\n";
+        }
+        res += "------------------------------------------\n";
+        return res;
+    }
+
+    public static Character fight(Character c1, Character c2) {
+
+        for (;;) {
+            c1.attack(c2);
+
+            if (c2.getCurrentHealth() == 0) {
+                return c1;
+            }
+            c2.attack(c1);
+
+            if (c1.getCurrentHealth() == 0) {
+                return c2;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.print(Character.printStatus());
+
+        Character aragorn = new Character("Aragorn", 20);
+        Character uruk = new Character("Uruk", 15);
+
+        System.out.print(Character.printStatus());
+
+        Character winner = Character.fight(aragorn, uruk);
+
+        System.out.println(winner.toString());
+        System.out.print(Character.printStatus());
+    }
+}
